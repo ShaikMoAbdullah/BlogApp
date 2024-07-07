79f3a7dce64fd4d53b9bcf710d85ec8feed10c2d
Shaik Mohammad Abdullah <smabdullahblr@gmail.com>
2024-07-07 23:04:47 +0530

    fix: build

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -31,23 +31,23 @@ export const addPost = (req, res) => {
   // jwt.verify(token, "jwtkey", (err, userInfo) => {
   //   if (err) return res.status(403).json("Token is not valid!");
 
-    const q =
-      "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`,`uid`) VALUES (?)";
+  const q =
+    "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`,`uid`) VALUES (?)";
 
-    const values = [
-      req.body.title,
-      req.body.desc,
-      req.body.img,
-      req.body.cat,
-      req.body.date,
-      userInfo.id,
-    ];
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.img,
+    req.body.cat,
+    req.body.date,
+    userInfo.id,
+  ];
 
-    db.query(q, [values], (err, data) => {
-      if (err) return res.status(500).json(err);
-      return res.json("Post has been created.");
-    });
+  db.query(q, [values], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.json("Post has been created.");
   });
+  // });
 };
 
 export const deletePost = (req, res) => {
@@ -57,15 +57,15 @@ export const deletePost = (req, res) => {
   // jwt.verify(token, "jwtkey", (err, userInfo) => {
   //   if (err) return res.status(403).json("Token is not valid!");
 
-    const postId = req.params.id;
-    const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
+  const postId = req.params.id;
+  const q = "DELETE FROM posts WHERE `id` = ? AND `uid` = ?";
 
-    db.query(q, [postId, userInfo.id], (err, data) => {
-      if (err) return res.status(403).json("You can delete only your post!");
+  db.query(q, [postId, userInfo.id], (err, data) => {
+    if (err) return res.status(403).json("You can delete only your post!");
 
-      return res.json("Post has been deleted!");
-    });
+    return res.json("Post has been deleted!");
   });
+  // });
 };
 
 export const updatePost = (req, res) => {
@@ -75,15 +75,15 @@ export const updatePost = (req, res) => {
   // jwt.verify(token, "jwtkey", (err, userInfo) => {
   //   if (err) return res.status(403).json("Token is not valid!");
 
-    const postId = req.params.id;
-    const q =
-      "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
+  const postId = req.params.id;
+  const q =
+    "UPDATE posts SET `title`=?,`desc`=?,`img`=?,`cat`=? WHERE `id` = ? AND `uid` = ?";
 
-    const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
+  const values = [req.body.title, req.body.desc, req.body.img, req.body.cat];
 
-    db.query(q, [...values, postId, userInfo.id], (err, data) => {
-      if (err) return res.status(500).json(err);
-      return res.json("Post has been updated.");
-    });
+  db.query(q, [...values, postId, userInfo.id], (err, data) => {
+    if (err) return res.status(500).json(err);
+    return res.json("Post has been updated.");
   });
+  // });
 };
