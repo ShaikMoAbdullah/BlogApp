a931d00f163b3e1f0801c8fcf29ef7da6685761d
Shaik Mohammad Abdullah <smabdullahblr@gmail.com>
2024-07-10 23:38:39 +0530

    fix

--- api/db.js
+++ api/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 export const db = mysql.createConnection({
   host: "blog-smabdullahblr-a807.h.aivencloud.com",
   user: "avnadmin",
-  password: process.env.DB_PASSWORD,
+  password: "AVNS_PTVC-si0k5sepAmbZU3",
   database: "defaultdb",
   port: "11821",
 });
