ecb37ea00ecb7b45c7e7802acfbdec54b09ae4bd
Shaik Mohammad Abdullah <smabdullahblr@gmail.com>
2024-07-10 21:48:45 +0530

    fix

--- api/index.js
+++ api/index.js
@@ -3,14 +3,31 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 import multer from "multer";
 import cors from "cors";
+import crypto from "crypto";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const secretKey =
+  process.env.SESSION_SECRET || crypto.randomBytes(64).toString("hex");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  session({
+    secret: secretKey,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../client/public/upload");
