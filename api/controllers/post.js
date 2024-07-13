4ea74e32bea40638c195100963212cd3dfe4430f
Shaik Mohammad Abdullah <smabdullahblr@gmail.com>
2024-07-13 16:57:01 +0530

    fix: category filter

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -3,9 +3,13 @@ import { db } from "../db.js";
 
 // Function to get all posts
 export const getPosts = (req, res) => {
-  const q = "SELECT * FROM posts";
-  db.query(q, (err, data) => {
-    if (err) return res.status(500).json(err);
+  const q = req.query.cat
+    ? "SELECT * FROM posts WHERE cat=?"
+    : "SELECT * FROM posts";
+
+  db.query(q, [req.query.cat], (err, data) => {
+    if (err) return res.status(500).send(err);
+
     return res.status(200).json(data);
   });
 };
